3da72eea83ea809cb2b7d40878d321a034397cf8
breakpoint <kl456123@outlook.com>
2020-04-28 04:27:30 +0800

    non-dma fbo matrix multiplication works

--- third_party/opengl/opengl/examples/fbo/fbo_es.cc
+++ third_party/opengl/opengl/examples/fbo/fbo_es.cc
@@ -1,5 +1,7 @@
 #define GLFW_INCLUDE_ES31
 #include <GLFW/glfw3.h>
+#include <string.h>
+#include <fstream>
 
 #include <cassert>
 #include <chrono>
@@ -21,27 +23,6 @@ GLuint program;
 struct Vertex {
     float x, y;
 };
-
-// This is the main part.
-static const char *fragment_shader_text = "#version 300 es\n"
-"precision mediump float;\n"
-"uniform sampler2D A;\n"
-"uniform sampler2D B;\n"
-"uniform int N;\n"
-"out float color;\n"
-"void main() {\n"
-"  ivec2 pixel = ivec2(gl_FragCoord.xy);\n"
-"  int idx = pixel.x;\n"
-"  int row = idx / N;\n"
-"  int col = idx % N;\n"
-"  color = 0.0;\n"
-"  for (int i = 0; i < N; i++) {\n"
-"    float a = texelFetch(A, ivec2(row * N + i, 0), 0).r;\n"
-"    float b = texelFetch(B, ivec2(i * N + col, 0), 0).r;\n"
-"    color += a * b;\n"
-"  }\n"
-"}\n";
-
 
 
 static constexpr size_t kNumVertices = 6;
@@ -71,10 +52,6 @@ inline const char *GLGetErrorString(GLenum error) {
             return "GL_INVALID_VALUE";
         case GL_INVALID_OPERATION:
             return "GL_INVALID_OPERATION";
-            //case GL_STACK_OVERFLOW:
-            // return "GL_STACK_OVERFLOW";
-            //case GL_STACK_UNDERFLOW:
-            // return "GL_STACK_UNDERFLOW";
         case GL_OUT_OF_MEMORY:
             return "GL_OUT_OF_MEMORY";
         default:
@@ -210,7 +187,19 @@ void glfw_destory(){
  * \param fragment_shader The **compiled** fragment shader.
  * \return The program ID.
  */
-void CreateProgram() {
+void CreateProgram(const std::string fname) {
+    std::ifstream fd(fname);
+    std::string src = std::string(std::istreambuf_iterator<char>(fd),
+            (std::istreambuf_iterator<char>()));
+    if(src.empty()){
+        std::cerr<<"Read File ERROR from "<<fname;
+    }
+
+    // create fragment shader and link it with vertex
+    // shader to create program
+    fragment_shader = CreateShader(GL_FRAGMENT_SHADER,
+            src.c_str());
+
     // Create the program and link the shaders.
     program = glCreateProgram();
     glAttachShader(program, vertex_shader_);
@@ -257,9 +246,9 @@ GLuint CreateTexture(const GLfloat *data, GLsizei width_, GLsizei height_){
     glBindTexture(GL_TEXTURE_2D, texture_);
 
     // Similar to cudaMemcpy.
-    OPENGL_CALL(glTexImage2D(GL_TEXTURE_2D, /*level=*/0, GL_RGBA32F,
+    OPENGL_CALL(glTexImage2D(GL_TEXTURE_2D, /*level=*/0, GL_R32F,
                 width_, height_, /*border=*/0,
-                GL_RGBA, GL_FLOAT, data));
+                GL_RED, GL_FLOAT, data));
     // TODO(zhixunt): What are these?
     OPENGL_CALL(
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
@@ -279,38 +268,65 @@ void SetInput( std::string name, GLuint id,  int tex_id){
     glBindTexture(GL_TEXTURE_2D, id);
 }
 
-void Download(GLfloat *data, GLint width, GLint height, GLuint texture,int tex_id){
-    OPENGL_CALL(glActiveTexture(GL_TEXTURE0 + tex_id));
-    OPENGL_CALL(glBindTexture(GL_TEXTURE_2D, texture));
-    glReadPixels(0, 0, width, height, GL_RGBA, GL_FLOAT, data);
+void Download(GLfloat *data, GLint width, GLint height, GLuint texture){
+    // OPENGL_CALL(glActiveTexture(GL_TEXTURE0 + tex_id));
+    // OPENGL_CALL(glBindTexture(GL_TEXTURE_2D, texture));
+    OPENGL_CALL(glReadBuffer(GL_COLOR_ATTACHMENT0));
+    OPENGL_CALL(glReadPixels(0, 0, width, height, GL_RED, GL_FLOAT, data));
+}
+
+void Download_DMA(GLfloat* data, GLint width, GLint height, GLuint texture){
+    size_t bytes = width*height*sizeof(float);
+
+    OPENGL_CALL(glReadBuffer(GL_COLOR_ATTACHMENT0));
+    OPENGL_CALL(glBindBuffer(GL_PIXEL_PACK_BUFFER, texture));
+    OPENGL_CALL(glBufferData(GL_PIXEL_PACK_BUFFER, bytes,
+            NULL, GL_STREAM_READ));
+    OPENGL_CALL(glReadPixels(0, 0, width, height, GL_RED, GL_FLOAT,
+            0));
+    void* mem = glMapBufferRange(GL_PIXEL_PACK_BUFFER, 0, bytes, GL_READ_ONLY);
+    assert(mem);
+    memcpy(data, mem, bytes);
+    glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+}
+
+void SetInt(const std::string name, int value){
+    glUniform1i(glGetUniformLocation(program, name.c_str()), value);
+}
+
+void ReadShader(const std::string fname){
+    std::ifstream fd(fname);
+    std::string src = std::string(std::istreambuf_iterator<char>(fd),
+            (std::istreambuf_iterator<char>()));
+    if(src.empty()){
+        std::cerr<<"Read File ERROR from "<<fname;
+    }
 }
 
 // void Render(GLuint Program, ){
 // }
 
 int main(){
-    const int N = 10;
+    const int N = 100;
     GLint width = N;
     GLint height = N;
-    const int niters = 10;
-    const size_t texture_size = width*height*4;
+    const int niters = 100;
+    const size_t num_elements = width*height;
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<float> dist(1.0f, 2.0f);
 
     glfw_init();
-    // create fragment shader and link it with vertex
-    // shader to create program
-    fragment_shader = CreateShader(GL_FRAGMENT_SHADER,
-            fragment_shader_text);
-    CreateProgram();
+
+    CreateProgram("../opengl/examples/fbo/fragment_es.glsl");
 
     //////////////////////////////////////////////////////
     // prepare data
     //////////////////////////////////////////////////////
-    std::vector<GLfloat> texture0_data(texture_size, 0.25f);
-    std::vector<GLfloat> texture1_data(texture_size, 0.25f);
-    for (size_t i = 0; i != texture_size; ++i) {
+    std::vector<GLfloat> texture0_data(num_elements, 0.25f);
+    std::vector<GLfloat> texture1_data(num_elements, 0.25f);
+    for (size_t i = 0; i != num_elements; ++i) {
         texture0_data[i] = dist(mt);
         texture1_data[i] = dist(mt);
     }
@@ -328,14 +344,15 @@ int main(){
     GLuint frame_buffer;
     OPENGL_CALL(glGenFramebuffers(1, &frame_buffer));
     OPENGL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, frame_buffer));
+    OPENGL_CALL(glViewport(0, 0, width, height));
     // Set "renderedTexture" as our colour attachement #0
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,GL_TEXTURE_2D,
             output , 0);
 
     // Set the list of draw buffers.
-    // GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+    GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
     // "1" is the size of DrawBuffers.
-    // OPENGL_CALL(glDrawBuffers(1, DrawBuffers));
+    OPENGL_CALL(glDrawBuffers(1, DrawBuffers));
 
     // Always check that our framebuffer is ok
     if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
@@ -346,8 +363,9 @@ int main(){
     // Tell the fragment shader what input textures to use.
     SetInput("A", input0, 0);
     SetInput("B", input1, 1);
-    OPENGL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, frame_buffer));
-    OPENGL_CALL(glViewport(0, 0, width, height));
+
+    // set uniform
+    SetInt("N", N);
 
     auto opengl_start = std::chrono::system_clock::now();
     for (int iter = 0; iter < niters; ++iter) {
@@ -356,7 +374,6 @@ int main(){
         glFinish();
     }
 
-    glDeleteFramebuffers(1, &frame_buffer);
 
     auto opengl_end = std::chrono::system_clock::now();
     std::cout << "opengl: "
@@ -365,8 +382,8 @@ int main(){
 
     ///////////////////////////////////////
     // Download output Data from Device
-    std::vector<GLfloat> retrieved_data(static_cast<size_t>(width * height));
-    // Download(retrieved_data.data(), width, height, output, );
+    std::vector<GLfloat> retrieved_data(static_cast<size_t>(num_elements));
+    Download_DMA(retrieved_data.data(), width, height, output);
     // for (GLuint unit = 0; unit != inputs.size(); ++unit) {
     // const std::string &name = inputs[unit].first;
     // Texture *texture = inputs[unit].second;
@@ -378,36 +395,38 @@ int main(){
     // OPENGL_CALL(glUniform1i(texture_uniform, unit));
     // }
 
-
-    // Always check that our framebuffer is ok
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-        std::cout << "Framebuffer not complete." << std::endl;
-        assert(false);
-    }
-
-    std::vector<GLfloat> cpu_result(static_cast<size_t>(texture_size));
+    std::vector<GLfloat> cpu_result(static_cast<size_t>(num_elements));
     auto cpu_start = std::chrono::system_clock::now();
-    // for (int iter = 0; iter < niters; ++iter) {
-    // for (int row = 0; row != N; ++row) {
-    // for (int col = 0; col != N; ++col) {
-    // cpu_result[row * N + col] = 0.0f;
-    // for (int i = 0; i != N; ++i) {
-    // GLfloat a = texture0_data[row * N + i];
-    // GLfloat b = texture1_data[i * N + col];
-    // cpu_result[row * N + col] += a * b;
-    // }
-    // }
-    // }
-    // }
+    for (int iter = 0; iter < niters; ++iter) {
+        for (int row = 0; row != N; ++row) {
+            for (int col = 0; col != N; ++col) {
+                cpu_result[row * N + col] = 0.0f;
+                for (int i = 0; i != N; ++i) {
+                    GLfloat a = texture0_data[row * N + i];
+                    GLfloat b = texture1_data[i * N + col];
+                    cpu_result[row * N + col] += a * b;
+                }
+            }
+        }
+    }
     auto cpu_end = std::chrono::system_clock::now();
 
-    // for (int i = 0; i < retrieved_data.size(); ++i) {
-    // assert(std::abs(retrieved_data[i] - cpu_result[i]) < 0.001f);
+    // just test instead of print for big matrix Multiplication
+    for (int i = 0; i < retrieved_data.size(); ++i) {
+        assert(std::abs(retrieved_data[i] - cpu_result[i]) < 0.001f);
+    }
+
+    // print to debug
+    // for(size_t i=0;i<retrieved_data.size();++i){
+    // std::cout<<retrieved_data[i]<<" ";
+    // std::cout<<cpu_result[i]<<" "<<std::endl;
     // }
 
     std::cout << "cpu:    "
         << ((cpu_end - cpu_start).count() / niters)
         << std::endl;
+
+    glDeleteFramebuffers(1, &frame_buffer);
 
     glfw_destory();
     return 0;
